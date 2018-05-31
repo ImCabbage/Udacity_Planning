2ec75f638910b67df73eba8bc9178145e2f1bed2
cabbage901214 <lvhao901214@163.com>
2018-06-01 00:14:56 +0800

    finish 2-11

--- FirstSearchProgram/quiz.py
+++ FirstSearchProgram/quiz.py
@@ -5,71 +5,81 @@ import numpy as np
 # 0 = Navigable space
 # 1 = Occupied space 
 grid = [[0, 0, 1, 0, 0, 0],
-		[0, 0, 1, 0, 0, 0],
-		[0, 0, 0, 0, 1, 0],
-		[0, 0, 1, 1, 1, 0],
-		[0, 0, 0, 0, 1, 0]]
+	[0, 0, 1, 0, 0, 0],
+	[0, 0, 0, 0, 1, 0],
+	[0, 0, 1, 1, 1, 0],
+	[0, 0, 0, 0, 1, 0]]
 
 init = [0, 0]
 goal = [len(grid)-1,len(grid[0])-1]
 cost = 1
 
 delta = [[-1, 0],   # go up
-		 [0, -1],   # go left
-		 [1, 0],	# go down
-		 [0, 1]]    # go right
+	 [0, -1],   # go left
+	 [1, 0],	# go down
+	 [0, 1]]    # go right
 
 delta_name = ['^', '<', 'V', '>']
 
-def search(grid, init, goal, cost):
-	# initialization
-	close_list = []
-	close_list.append(init)
-
-	# generate heuristic matrix
-	H = np.zeros((len(grid), len(grid[0])), dtype=float)
-	for i in range(len(grid)):
-		for j in range(len(grid[0])):
-			H[i,j] = (goal[0] - i) + (goal[1] - j)
-
-	while (close_list[-1] != goal):
-		# check available points
-		open_list = []
-		for i in range(4):
-			# get the coordinates of the potential waypoints
-			tmp_point = [close_list[-1][0] + delta[i][0], close_list[-1][1] + delta[i][1]]
-			# Step #1: check all the potential waypoints which are in the maze
-			if ((tmp_point[0] >= 0) and (tmp_point[1] >= 0)):
-				# Step #2: check all the potential waypoints which are not in the close list
-				for j in range(len(close_list)):
-					if ((tmp_point[0] != close_list[j][0]) and (tmp_point[1] != close_list[j][1])):
-						open_list.append(tmp_point)
-		
-
-		open_list.append(tmp_point)
-
-		# select the optimal waypoint
-		F = 999
-		index = 999
-		for i in range(len(open_list)):
-			tmp_point = open_list[i];
-			tmp_F = cost + H[tmp_point[0], tmp_point[1]]
-			if tmp_F < F:
-				F = tmp_F
-				index = i
-		
-		# add the waypoint to list 
-		close_list.append(open_list[index])
-
-		# print close_list
-		# print "\n"
+def search():
+	# closed = [[0 for row in range(len(grid[0]))] for col in range(len(grid))]
+	closed = np.zeros((len(grid), len(grid[0])), dtype=float)
+	expand = (-1)*np.ones((len(grid), len(grid[0])), dtype=float)
+	action = (-1)* np.ones((len(grid), len(grid[0])), dtype=float)
 
 
+	closed[init[0]][init[1]] = 1
+	expand[init[0]][init[1]] = 0
+	cnt = 1
+
+	x = init[0]
+	y = init[1]
+	g = 0
+
+	open_list = [[g, x, y]]
+
+	found = False
+	resign = False
+
+	while found is False and resign is False:
+
+		if len(open_list) == 0:
+			resign = True
+			print 'fail'
+		else:
+			open_list.sort()
+			open_list.reverse()
+			next_ = open_list.pop()
+			x = next_[1]
+			y = next_[2]
+			g = next_[0]
+
+			if x == goal[0] and y == goal[1]:
+				found = True
+				print next_
+			else:
+				for i in range(len(delta)):
+					x2 = x + delta[i][0]
+					y2 = y + delta[i][1]
+					if x2  >= 0 and x2 < len(grid) and y2 >= 0 and y2 < len(grid[0]):
+						if closed[x2][y2] == 0 and grid[x2][y2] == 0:
+							g2 = g + cost
+							open_list.append([g2, x2, y2])
+							closed[x2][y2] = 1
+							expand[x2][y2] = cnt
+							action[x2][y2] = i
+							cnt += 1
+	print expand
+	print action
 
 
 
 
 
 
-if __name__ == '__main__':
-	search(grid, init, goal, cost)
+
+
+
+
+
+search()
